158991273ee1448d39c19af20ffd36652a0923cf
retttddd <ivan.stsepaniuk@gmail.com>
2023-03-01 23:04:32 +0100

    middle result

---
+++ cli/root.go
@@ -0,0 +1,23 @@
+package cli
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "operator",
+	Short: "operator stores data secretly",
+	Long:  `q`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
-import (
-	"awesomeProject3/service"
-	"awesomeProject3/service/de_encoding"
-	"awesomeProject3/storage"
-	"fmt"
-)
-
 func main() {
-	passWord := "hello"
-	srv := service.New(storage.New(), de_encoding.NewAESEncoder(de_encoding.PassToSecretKey(passWord)))
-	//srv.WriteSecret("key18", "ThatsValue18")
-	//srv.WriteSecret("key19", "THatsValue19")
-	//srv.WriteSecret("key20", "ThatsValue20")
-	//srv.WriteSecret("key21", "THatsValue21")
-	value, _ := srv.ReadSecret("key19")
-	fmt.Print(value)
+	//passWord := "hello2"
+	//srv := service.New(storage.New(), de_encoding.NewAESEncoder(de_encoding.PassToSecretKey(passWord)))
+	////srv.WriteSecret("key18", "ThatsValue18") //hello
+	////srv.WriteSecret("key19", "THatsValue19") //hello
+	////srv.WriteSecret("key20", "ThatsValue20") //hello
+	////srv.WriteSecret("key21", "THatsValue21") //hello
+	////srv.WriteSecret("key22", "THatsValue22") //hello2
+	//value, _ := srv.ReadSecret("key22")
+	//fmt.Print(value)
+	operator.Execute()
 
 }
