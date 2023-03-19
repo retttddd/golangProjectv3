6ae88d2d29939a6d6927379a0c5de3a8c2225d07
jan <ivan.stseoaniuk@gmail.com>
2023-03-20 00:30:06 +0100

    test cases for aesEncoder and simpleSecretService

--- service/ciphering/aesEncoder_test.go
+++ service/ciphering/aesEncoder_test.go
@@ -7,54 +7,90 @@ import (
 )
 
 const isBase64 = "^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$"
-const encrpyptedString = "848d56796ac855b0b3f06cb77bb652d2a9a4b84f64026dbfbfe94aaf76cb843608c85a2112ac"
+const encryptedString = "848d56796ac855b0b3f06cb77bb652d2a9a4b84f64026dbfbfe94aaf76cb843608c85a2112ac"
 const sourceString = "testString"
 const key = "1234567890123456"
+const wrongKey = "23"
 
-func TestAesEncoder_Encrypt(t *testing.T) {
-	encoder := NewAESEncoder()
-	result, err := encoder.Encrypt(sourceString, []byte(key))
-
-	require.Nil(t, err)
-	require.Regexp(t, isBase64, result)
+var encryptionTestCases = []struct {
+	name      string
+	source    string
+	key       string
+	expected  string
+	expectErr bool
+}{
+	{
+		name:      "encryption valid case",
+		source:    sourceString,
+		key:       key,
+		expected:  sourceString,
+		expectErr: false,
+	},
+	{
+		name:      "encryptiom wrong key size",
+		source:    sourceString,
+		key:       wrongKey,
+		expected:  "",
+		expectErr: true,
+	},
 }
 
-func TestAesEncoder_Decrypt(t *testing.T) {
-	decoder := NewAESEncoder()
-	result, err := decoder.Decrypt(encrpyptedString, []byte(key))
-
-	require.Nil(t, err)
-	require.Equal(t, sourceString, result)
+func TestAesEncoder_Encrypt_TableDriven(t *testing.T) {
+	for _, tc := range encryptionTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			encoder := NewAESEncoder()
+			result, err := encoder.Encrypt(tc.source, []byte(tc.key))
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.Nil(t, err)
+				require.Regexp(t, isBase64, result)
+			}
+		})
+	}
 }
 
-func TestAesEncoder_EncryptWithCorruptedKey(t *testing.T) {
-	const WrongKeySizeError = "crypto/aes: invalid key size"
-	const wrongKey = "23"
-
-	encoder := NewAESEncoder()
-	result, err := encoder.Encrypt(sourceString, []byte(wrongKey))
-
-	require.Empty(t, result)
-	require.ErrorContains(t, err, WrongKeySizeError)
+var decryptionTestCases = []struct {
+	name      string
+	encrypted string
+	key       string
+	expected  string
+	expectErr bool
+}{
+	{
+		name:      "valid case",
+		encrypted: encryptedString,
+		key:       key,
+		expected:  sourceString,
+		expectErr: false,
+	},
+	{
+		name:      "wrong key size",
+		encrypted: encryptedString,
+		key:       "wrongkey",
+		expected:  "",
+		expectErr: true,
+	},
+	{
+		name:      "corrupted data",
+		encrypted: "zzz",
+		key:       key,
+		expected:  "",
+		expectErr: true,
+	},
 }
 
-func TestAesEncoder_DecryptWithCorruptedData(t *testing.T) {
-	const CorruptedSourceStringError = "encoding/hex: invalid byte:"
-	const corruptedEncryptedString = "2g" //does not represent base 16 numbers which are needed to complete
-
-	encoder := NewAESEncoder()
-	result, err := encoder.Decrypt(corruptedEncryptedString, []byte(key))
-
-	require.Empty(t, result)
-	require.ErrorContains(t, err, CorruptedSourceStringError)
-}
-func TestAesEncoder_DecryptCorruptedKey(t *testing.T) {
-	const WrongKeySizeError = "crypto/aes: invalid key size"
-	const wrongKey = "23"
-
-	decoder := NewAESEncoder()
-	result, err := decoder.Decrypt(encrpyptedString, []byte(wrongKey))
-
-	require.Empty(t, result)
-	require.ErrorContains(t, err, WrongKeySizeError)
+func TestAesEncoder_Decrypt_TableDriven(t *testing.T) {
+	for _, tc := range decryptionTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			decoder := NewAESEncoder()
+			result, err := decoder.Decrypt(tc.encrypted, []byte(tc.key))
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.expected, result)
+			}
+		})
+	}
 }
--- service/simpleSecretService_test.go
+++ service/simpleSecretService_test.go
@@ -1,7 +1,77 @@
 package service
 
-import "testing"
+import (
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+const notSecretVal = "decryptedValue"
+const secretVal = "encryptedValue"
+const secretVal2 = "encryptedValue2"
+
+type mockStorage struct{ mock.Mock }
+
+func newMockStorage() *mockStorage { return &mockStorage{} }
+
+func (m *mockStorage) Read(key string) (string, error) {
+	arg := m.Called(key)
+	return arg.String(0), arg.Error(1)
+}
+
+func (m *mockStorage) Write(key string, value string) error {
+	arg := m.Called(key, value)
+	return arg.Error(0)
+}
+
+type mockEncoder struct{ mock.Mock }
+
+func newMockEncoder() *mockEncoder { return &mockEncoder{} }
+
+func (m *mockEncoder) Encrypt(plaintext string, cipherKey []byte) (string, error) {
+	arg := m.Called(plaintext, cipherKey)
+	return arg.String(0), arg.Error(1)
+}
+func (m *mockEncoder) Decrypt(ct string, cipherKey []byte) (string, error) {
+	arg := m.Called(ct, cipherKey)
+	return arg.String(0), arg.Error(1)
+}
+
+//On("Encrypt", "my-value", mock.AnythingOfType("[]uint8")).Return([]byte("encrypted-value"), nil)
+//On("Encrypt", "my-key", mock.AnythingOfType("[]uint8")).Return([]byte("encrypted-key"), nil)
+//.On("Write", []byte("encrypted-key"), []byte("encrypted-value")).Return(nil)
 
 func TestSimpleSecretService_WriteSecret(t *testing.T) {
+	me := newMockEncoder()
+	me.On("Encrypt", notSecretVal, mock.Anything).Return(secretVal, nil)
+	me.On("Encrypt", notSecretVal, mock.Anything).Return(secretVal2, nil)
+
+	ms := newMockStorage()
+	ms.On("Write", secretVal, secretVal2).Return(nil)
+
+	ss := New(ms, me, me)
+	err := ss.WriteSecret("key", notSecretVal, "qweqw")
+	require.Nil(t, err)
+
+	//m.AssertNumberOfCalls(t, "Encrypt", 2)
 
 }
+
+func TestSimpleSecretService_ReadSecret(t *testing.T) {
+	const read = "Read"
+	const valFromStorage = "valueFromStorage"
+	ms := newMockStorage()
+	ms.On(read, secretVal).Return(valFromStorage, nil)
+
+	me := newMockEncoder()
+	me.On("Encrypt", notSecretVal, mock.Anything).Return(secretVal, nil)
+	me.On("Decrypt", valFromStorage, mock.Anything).Return(notSecretVal, nil)
+
+	ss := New(ms, me, me)
+	result, err := ss.ReadSecret(notSecretVal, "qweqw")
+	require.Nil(t, err)
+	require.NotNil(t, result)
+	require.Equal(t, result, notSecretVal)
+}
+
+var m mockStorage
